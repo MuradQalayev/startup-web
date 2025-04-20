05c68f62780e2444ddd7ba1d8e61992f22096db0
Murad <s329987@studenti.polito.it>
2025-04-20 14:16:23 +0200

    search form and modifications on Home page component

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,9 +1,18 @@
+import SearchForm from "../../components/SearchForm";
 
-export default function Home() {
+export default async function Home({searchParams}) {
+
+  const query = searchParams?.query
+
+
   return (
     <>
       <section className="pink_container">
         <h1 className="heading">Pitch your startup, <br/> Connect with, entrepreneurs</h1>
+        <p className="sub-heading !max-w-3xl"> 
+          Submit ideas, Vote on Pitches and Get Noticed in Virtual
+        </p>
+        <SearchForm query ={query}/>
       </section>
     </>
   );
